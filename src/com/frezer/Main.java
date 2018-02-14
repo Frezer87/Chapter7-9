23a053935461f4654a7d19a8bffc18371931a98c
Frezer Gebremeskel <frezer.kidane@gmail.com>
2018-02-14 01:35:12 -0500

    Initial commit

---
+++ src/com/frezer/Main.java
@@ -0,0 +1,28 @@
+package com.frezer;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double[] numbers = new double[10];
+
+        System.out.println("Enter ten numbers: ");
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = input.nextDouble();
+        }
+
+
+        System.out.println("The minimum number is: " + min(numbers));
+    }
+
+
+    public static double min(double[] array) {
+        double min = array[0];
+        for (double i : array) {
+            if (i < min) min = i;
+        }
+        return min;
+    }
+}
